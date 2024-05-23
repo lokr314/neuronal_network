ea1b148ac6e9df212d2c0d33f5760703aefd39b4
unschlagbar <adrian@kuhlmann@gmx.de>
2024-05-23 13:28:30 +0200

    Moin

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use neuronal_network::NeuronalNetwork;
 mod neuronal_network;
 
 fn main() {
-    let v = vec![ 0, 3, 4];
+    let v = vec![2, 3, 3, 5, 2];
     let k = NeuronalNetwork::new(v);
+
+    println!("{:?}", k);
 }
--- src/neuronal_network.rs
+++ src/neuronal_network.rs
@@ -1,11 +1,37 @@
+use std::fmt::Debug;
+
+
 
 #[derive(Debug)]
 pub struct NeuronalNetwork {
-    pub layout: Vec<u16>
+    pub layout: Vec<u16>,
+    layers: Vec<Layer>
 }
 
 impl NeuronalNetwork {
     pub fn new(layout: Vec<u16>) -> Self {
-        NeuronalNetwork { layout }
+        let mut layers = Vec::with_capacity(layout.len() - 1);
+
+        for i in 1..layout.len() {
+            layers.push(Layer::new(layout[i - 1], layout[i]));
+        }
+
+        NeuronalNetwork { layout, layers }
+    }
+}
+
+
+#[derive(Debug)]
+struct Layer {
+    weights: Vec<Vec<f32>>,
+    biases: Vec<f32>
+}
+
+impl Layer {
+    fn new(inputs: u16, neurons: u16) -> Self {
+        let weights: Vec<Vec<f32>> = vec![vec![0.0; inputs as usize]; neurons as usize];
+        let biases = vec![0.0; neurons as usize];
+
+        Layer { weights, biases }
     }
 }
